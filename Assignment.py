d0eeee59337217e14ea4af1bd653c3eee96f7f75
Your Name <vincentyang2001@gmail.com>
2019-05-20 21:36:41 -0400

    Finish base of game

---
+++ Assignment.py
@@ -0,0 +1,83 @@
+import arcade
+import random
+import math
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+
+class ShipSprite(arcade.Sprite):
+
+    def __init__(self, filename, scale):
+        super().__init__(filename, scale)
+
+        self.center_x = 0
+        self.center_y = 0
+
+    def update(self):
+        # Move the avatar
+        self.center_y += self.change_y
+        self.center_x += self.change_x
+
+        # See if the avatar hit the edge of the screen. If so, change direction
+        if self.center_x < 25:
+            self.center_x = 25
+
+        if self.center_x > SCREEN_WIDTH - 25:
+            self.center_x = SCREEN_WIDTH - 25
+
+        if self.center_y < 25:
+            self.center_y = 25
+
+        if self.center_y > SCREEN_HEIGHT - 25:
+            self.center_y = SCREEN_HEIGHT - 25
+
+
+class MyGame(arcade.Window):
+
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+
+        self.player_list = None
+        self.player_sprite = None
+
+    def setup(self):
+        self.player_list = arcade.SpriteList()
+        self.player_sprite = ShipSprite(r'images/plane.png', 0.075)
+        self.player_sprite.center_x = 400
+        self.player_sprite.center_y = 300
+        self.player_list.append(self.player_sprite)
+
+    def on_draw(self):
+        arcade.start_render()
+        self.player_list.draw()
+
+    def on_key_press(self, key, modifiers):
+        # User Control with arrow keys
+        if key == arcade.key.LEFT:
+            self.player_sprite.change_x = -10
+        elif key == arcade.key.RIGHT:
+            self.player_sprite.change_x = 10
+        elif key == arcade.key.UP:
+            self.player_sprite.change_y = 10
+        elif key == arcade.key.DOWN:
+            self.player_sprite.change_y = -10
+
+    def on_key_release(self, key, modifiers):
+        # Releasing of arrow keys
+        if key == arcade.key.LEFT or key == arcade.key.RIGHT:
+            self.player_sprite.change_x = 0
+        elif key == arcade.key.UP or key == arcade.key.DOWN:
+            self.player_sprite.change_y = 0
+
+    def update (self, x):
+        self.player_sprite.update()
+
+
+def main():
+    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, "Game")
+    window.setup()
+    arcade.run()
+
+main()
+
