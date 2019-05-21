1943e2aee4658905a8d864b4f5f0479d861919c0
Your Name <vincentyang2001@gmail.com>
2019-05-21 09:35:00 -0400

    Finish player sprite

--- Assignment.py
+++ Assignment.py
@@ -6,6 +6,19 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 
+class Bullets(arcade.Sprite):
+
+    def __init__(self, filename, scale):
+        super().__init__(filename, scale)
+        self.speed = 0
+
+    def update(self):
+        self.change_x = -math.sin(math.radians(self.angle)) * self.speed
+        self.change_y = math.cos(math.radians(self.angle)) * self.speed
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+
 class ShipSprite(arcade.Sprite):
 
     def __init__(self, filename, scale):
@@ -13,11 +26,9 @@ class ShipSprite(arcade.Sprite):
 
         self.center_x = 0
         self.center_y = 0
+        self.speed = 0
 
     def update(self):
-        # Move the avatar
-        self.center_y += self.change_y
-        self.center_x += self.change_x
 
         # See if the avatar hit the edge of the screen. If so, change direction
         if self.center_x < 25:
@@ -31,6 +42,14 @@ class ShipSprite(arcade.Sprite):
 
         if self.center_y > SCREEN_HEIGHT - 25:
             self.center_y = SCREEN_HEIGHT - 25
+
+        # Move the avatar
+        self.change_x = -math.sin(math.radians(self.angle)) * self.speed
+        self.change_y = math.cos(math.radians(self.angle)) * self.speed
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+        super().update()
 
 
 class MyGame(arcade.Window):
@@ -55,22 +74,22 @@ class MyGame(arcade.Window):
     def on_key_press(self, key, modifiers):
         # User Control with arrow keys
         if key == arcade.key.LEFT:
-            self.player_sprite.change_x = -10
+            self.player_sprite.change_angle = 3
         elif key == arcade.key.RIGHT:
-            self.player_sprite.change_x = 10
+            self.player_sprite.change_angle = -3
         elif key == arcade.key.UP:
-            self.player_sprite.change_y = 10
+            self.player_sprite.speed = 5
         elif key == arcade.key.DOWN:
-            self.player_sprite.change_y = -10
+            self.player_sprite.speed = -5
 
     def on_key_release(self, key, modifiers):
         # Releasing of arrow keys
         if key == arcade.key.LEFT or key == arcade.key.RIGHT:
-            self.player_sprite.change_x = 0
+            self.player_sprite.change_angle = 0
         elif key == arcade.key.UP or key == arcade.key.DOWN:
-            self.player_sprite.change_y = 0
+            self.player_sprite.speed = 0
 
-    def update (self, x):
+    def update(self, x):
         self.player_sprite.update()
 
 
