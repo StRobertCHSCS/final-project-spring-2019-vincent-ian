d5af61a23337f15f962d91b38b89f6e71bfca9b2
Your Name <vincentyang2001@gmail.com>
2019-05-26 20:12:51 -0400

    Finish bullet sprite and player sprite

--- Assignment.py
+++ Assignment.py
@@ -11,12 +11,14 @@ class Bullets(arcade.Sprite):
     def __init__(self, filename, scale):
         super().__init__(filename, scale)
         self.speed = 0
+        self.center_x = 0
+        self.center_y = 0
 
     def update(self):
-        self.change_x = -math.sin(math.radians(self.angle)) * self.speed
-        self.change_y = math.cos(math.radians(self.angle)) * self.speed
         self.center_x += self.change_x
         self.center_y += self.change_y
+        self.angle = math.degrees(math.atan2(self.change_y, self.change_x))
+        super().update()
 
 
 class ShipSprite(arcade.Sprite):
@@ -48,7 +50,6 @@ class ShipSprite(arcade.Sprite):
         self.change_y = math.cos(math.radians(self.angle)) * self.speed
         self.center_x += self.change_x
         self.center_y += self.change_y
-
         super().update()
 
 
@@ -59,6 +60,8 @@ class MyGame(arcade.Window):
 
         self.player_list = None
         self.player_sprite = None
+        self.bullet_list = None
+        self.bullet_sprite = None
 
     def setup(self):
         self.player_list = arcade.SpriteList()
@@ -67,12 +70,24 @@ class MyGame(arcade.Window):
         self.player_sprite.center_y = 300
         self.player_list.append(self.player_sprite)
 
+        self.bullet_list = arcade.SpriteList()
+
     def on_draw(self):
         arcade.start_render()
         self.player_list.draw()
+        self.bullet_list.draw()
 
     def on_key_press(self, key, modifiers):
         # User Control with arrow keys
+
+        if key == arcade.key.SPACE:
+            self.bullet_sprite = Bullets(r'images/lazer.png', 0.25)
+            self.bullet_sprite.center_x = self.player_sprite.center_x
+            self.bullet_sprite.center_y = self.player_sprite.center_y
+            self.bullet_sprite.change_y = math.cos(math.radians(self.player_sprite.angle)) * 13
+            self.bullet_sprite.change_x = -math.sin(math.radians(self.player_sprite.angle)) * 13
+            self.bullet_list.append(self.bullet_sprite)
+
         if key == arcade.key.LEFT:
             self.player_sprite.change_angle = 3
         elif key == arcade.key.RIGHT:
@@ -91,6 +106,7 @@ class MyGame(arcade.Window):
 
     def update(self, x):
         self.player_sprite.update()
+        self.bullet_list.update()
 
 
 def main():
