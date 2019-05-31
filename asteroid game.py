0ec37c68f3579b59bd8a78f3efa46564d3173dbd
I-Lin <ian.lin20@ycdsbk12.ca>
2019-05-31 11:01:05 -0400

    Add collision detection for asteroid and bullet sprites

--- asteroid game.py
+++ asteroid game.py
@@ -87,6 +87,7 @@ class MyGame(arcade.Window):
         self.bullet_list = None
         self.bullet_sprite = None
 
+        self.score = 0
         self.game_level = 1
 
         # Sprite lists
@@ -181,6 +182,12 @@ class MyGame(arcade.Window):
         # Draw background
         arcade.draw_texture_rectangle(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
 
+        # Display lives
+        arcade.draw_text("Lives: " + str(self.player_sprite.lives), 50, 50, arcade.color.WHITE, 30)
+
+        # Display score
+        arcade.draw_text("Score: " + str(self.score), 50, 100, arcade.color.WHITE, 30)
+
         self.player_list.draw()
         self.bullet_list.draw()
 
@@ -224,38 +231,106 @@ class MyGame(arcade.Window):
             self.player_sprite.speed = 0
 
     def update(self, x):
-        self.player_sprite.update()
-        self.bullet_list.update()
+        if self.player_sprite.lives != 0:
+            self.player_sprite.update()
+            self.bullet_list.update()
 
-        if self.game_level == 1:
-            self.level_1_asteroid_list.update()
+            if self.game_level == 1:
+                self.level_1_asteroid_list.update()
 
-            # Check if player has hit sushi
-            level_1_asteroid_hit_list = arcade.check_for_collision_with_list(self.player_sprite,
-                                                                             self.level_1_asteroid_list)
-            for asteroid in level_1_asteroid_hit_list:
-                asteroid.kill()
-                self.player_sprite.lives -= 1
-            if len(level_1_asteroid_hit_list) == 5:
-                self.game_level = 2
+                # Check if player has hit asteroid
+                level_1_asteroid_hit_list = arcade.check_for_collision_with_list(self.player_sprite,
+                                                                                 self.level_1_asteroid_list)
+                for asteroid in level_1_asteroid_hit_list:
+                    asteroid.kill()
+                    self.player_sprite.lives -= 1
 
-        elif self.game_level == 2:
-            self.level_2_asteroid_list.update()
+                if len(self.bullet_list) > 0:
+                    level_1_asteroid_shot_list = arcade.check_for_collision_with_list(self.bullet_sprite,
+                                                                                      self.level_1_asteroid_list)
+                    for asteroid in level_1_asteroid_shot_list:
+                        asteroid.kill()
+                        self.score += 1
 
-            # Check if player has hit sushi
-            level_2_asteroid_hit_list = arcade.check_for_collision_with_list(self.player_sprite,
-                                                                             self.level_1_asteroid_list)
-            for asteroid in level_2_asteroid_hit_list:
-                asteroid.kill()
-            if len(level_2_asteroid_hit_list) == 0:
-                self.game_level = 3
+                    if len(self.level_1_asteroid_list) == 0:
+                        self.game_level = 2
 
-        elif self.game_level == 3:
-            self.level_3_asteroid_list.update()
-        elif self.game_level == 4:
-            self.level_4_asteroid_list.update()
-        elif self.game_level == 5:
-            self.level_5_asteroid_list.update()
+            elif self.game_level == 2:
+                self.level_2_asteroid_list.update()
+
+                # Check if player has hit asteroid
+                level_2_asteroid_hit_list = arcade.check_for_collision_with_list(self.player_sprite,
+                                                                                 self.level_2_asteroid_list)
+                for asteroid in level_2_asteroid_hit_list:
+                    asteroid.kill()
+                    self.player_sprite.lives -= 1
+
+                if len(self.bullet_list) > 0:
+                    level_2_asteroid_shot_list = arcade.check_for_collision_with_list(self.bullet_sprite,
+                                                                                      self.level_2_asteroid_list)
+                    for asteroid in level_2_asteroid_shot_list:
+                        asteroid.kill()
+                        self.score += 1
+
+                    if len(self.level_2_asteroid_list) == 0:
+                        self.game_level = 3
+
+            elif self.game_level == 3:
+                self.level_3_asteroid_list.update()
+
+                # Check if player has hit asteroid
+                level_3_asteroid_hit_list = arcade.check_for_collision_with_list(self.player_sprite,
+                                                                                 self.level_3_asteroid_list)
+                for asteroid in level_3_asteroid_hit_list:
+                    asteroid.kill()
+                    self.player_sprite.lives -= 1
+
+                if len(self.bullet_list) > 0:
+                    level_3_asteroid_shot_list = arcade.check_for_collision_with_list(self.bullet_sprite,
+                                                                                      self.level_3_asteroid_list)
+                    for asteroid in level_3_asteroid_shot_list:
+                        asteroid.kill()
+                        self.score += 1
+
+                    if len(self.level_3_asteroid_list) == 0:
+                        self.game_level = 4
+
+            elif self.game_level == 4:
+                self.level_4_asteroid_list.update()
+
+                # Check if player has hit asteroid
+                level_4_asteroid_hit_list = arcade.check_for_collision_with_list(self.player_sprite,
+                                                                                 self.level_4_asteroid_list)
+                for asteroid in level_4_asteroid_hit_list:
+                    asteroid.kill()
+                    self.player_sprite.lives -= 1
+
+                if len(self.bullet_list) > 0:
+                    level_4_asteroid_shot_list = arcade.check_for_collision_with_list(self.bullet_sprite,
+                                                                                      self.level_4_asteroid_list)
+                    for asteroid in level_4_asteroid_shot_list:
+                        asteroid.kill()
+                        self.score += 1
+
+                    if len(self.level_4_asteroid_list) == 0:
+                        self.game_level = 5
+
+            elif self.game_level == 5:
+                self.level_5_asteroid_list.update()
+
+                # Check if player has hit asteroid
+                level_5_asteroid_hit_list = arcade.check_for_collision_with_list(self.player_sprite,
+                                                                                 self.level_5_asteroid_list)
+                for asteroid in level_5_asteroid_hit_list:
+                    asteroid.kill()
+                    self.player_sprite.lives -= 1
+
+                if len(self.bullet_list) > 0:
+                    level_5_asteroid_shot_list = arcade.check_for_collision_with_list(self.bullet_sprite,
+                                                                                      self.level_5_asteroid_list)
+                    for asteroid in level_5_asteroid_shot_list:
+                        asteroid.kill()
+                        self.score += 1
 
 
 def main():
